c5262d79a85191b90d0b2b6c26535d91dca67d01
Ivan_Selivanov <shertors@gmail.com>
2022-08-23 17:03:26 +0300

    boxicons support

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css'
+import './boxicons.min.css'
 
 function App() {
-	return <div className="App">Hello</div>
+	return (
+		<div className="App">
+			Hello <i class="bx bxl-mongodb"></i>
+		</div>
+	)
 }
 
 export default App
