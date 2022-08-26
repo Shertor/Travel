9b2786b24353cdf2eab737e9a3ebf1e805d71429
Ivan_Selivanov <shertors@gmail.com>
2022-08-26 17:33:21 +0300

    Home - mobile - ios fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useEffect } from 'react'
 import './App.css'
 import './boxicons.min.css'
 
 import Navigation from './Components/Navigation/Navigation'
 
 function App() {
+	const appHeight = () => {
+		const doc = document.documentElement
+		doc.style.setProperty('--app-height', `${window.innerHeight}px`)
+	}
+
+	useEffect(() => {
+		window.addEventListener('resize', appHeight)
+		appHeight()
+	}, [])
+
 	return (
 		<div className="App">
 			<Navigation />
