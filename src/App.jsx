8b3fcb693232898c12c068cf8d5bde8487532556
Ivan_Selivanov <shertors@gmail.com>
2022-09-01 16:57:58 +0300

    Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import './boxicons.min.css'
 
+import IsMobile from './Utils/IsMobile'
 import Navigation from './Components/Navigation/Navigation'
 import Footer from './Components/Footer/Footer'
-import IsMobile from './Utils/IsMobile'
 
 function App() {
 	const appHeight = () => {
